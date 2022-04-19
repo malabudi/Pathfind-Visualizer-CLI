34ba063227ff893f86b0c99d2d8056755269e2ef
Mohamad Alabudi <80290328+malabudi@users.noreply.github.com>
2022-04-19 18:51:57 -0400

    Changed all tuples to pairs
    Pairs will help represent x and y coordinates easier for the map

--- Pathfinder.h
+++ Pathfinder.h
@@ -1,5 +1,6 @@
-#include <tuple>
+#include <utility>
 #include <vector>
+#include <map>
 
 #ifndef PATHFINDER_H
 #define PATHFINDER_H
@@ -8,14 +9,32 @@
 class PathFinder
 {
     private:
-        std::tuple<int> start;
-        std::tuple<int> goal;
+        std::pair<int, int> start;
+        std::pair<int, int> goal;
         std::vector<std::vector<char>> boardMap;
 
-    public:
-        // This is where all children will implement their respective algorithms to pathfind
-        virtual void findPath(const std::tuple<int> start, const std::tuple<int> goal, std::vector<std::vector<char>>& boardMap) = 0;
+        static const std::map< std::string, std::pair<int, int> > offsetCoord;
+
+    // All gets and sets will be in protected for the parent class
+    protected:
+        std::vector<std::vector<char>> getBoardMap();
+        std::pair<int, int> getStart();
+        std::pair<int, int> getGoal();
+
         void setBoardMap(std::vector<std::vector<char>> boardMap);
+        void setStart(std::pair<int, int> start);
+        void setGoal(std::pair<int, int> goal);
+
+        std::vector< std::pair<int, int> > createPath(const std::pair<int, int>& start, std::pair<int, int>& goal, const std::map< std::pair<int, int>, std::pair<int, int> >& coordsDiscovered);
+
+    public:
+        PathFinder(std::pair<int, int> start, std::pair<int, int> goal, std::vector<std::vector<char>> boardMap);
+
+        // This is where all children will implement their respective algorithms | Abstract member function
+        virtual std::vector< std::pair<int, int> > findPath() = 0;
+
+        bool isValidPosition(std::pair<int, int> position, std::vector<std::vector<char>> boardMap);
+
 };
 
 #endif // PATHFINDER_H
